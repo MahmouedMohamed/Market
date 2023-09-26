8faf0189c00158e5982ae58df1985691dbf1dc67
MahmouedMohamed <mahmouedmartin222@yahoo.com>
2023-09-26 20:49:44 +0300

    Add Views Styling, JS

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,30 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+let mouseDown = false;
+let startX, scrollLeft;
+const slider = document.querySelector('.scrollbar');
+
+const startDragging = (e) => {
+  mouseDown = true;
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+}
+
+const stopDragging = (e) => {
+  mouseDown = false;
+}
+
+const move = (e) => {
+  e.preventDefault();
+  if(!mouseDown) { return; }
+  const x = e.pageX - slider.offsetLeft;
+  const scroll = x - startX;
+  slider.scrollLeft = scrollLeft - scroll;
+}
+
+// Add the event listeners
+slider.addEventListener('mousemove', move, false);
+slider.addEventListener('mousedown', startDragging, false);
+slider.addEventListener('mouseup', stopDragging, false);
+slider.addEventListener('mouseleave', stopDragging, false);
