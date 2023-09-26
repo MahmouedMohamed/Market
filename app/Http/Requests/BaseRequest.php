d56fbca5a8e470af5333cbba857343c69ef5d9fe
MahmouedMohamed <mahmouedmartin222@yahoo.com>
2023-09-26 20:48:11 +0300

    Add store product request

---
+++ app/Http/Requests/BaseRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Traits\ApiResponse;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class BaseRequest extends FormRequest
+{
+    use ApiResponse;
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'err_flag' => true,
+            'message' => __('General.ValidationFailure'),
+            'data' => null,
+            'errors' => $validator->errors(),
+        ], 400));
+    }
+}
