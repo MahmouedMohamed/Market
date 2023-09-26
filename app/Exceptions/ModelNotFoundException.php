4410abeb4d49f11cddaf3973fff8828e5e30de40
MahmouedMohamed <mahmouedmartin222@yahoo.com>
2023-09-26 20:44:01 +0300

    Add Model Not Found Exception

---
+++ app/Exceptions/ModelNotFoundException.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+
+class ModelNotFoundException extends Exception
+{
+    /**
+     * Render the exception as an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function render()
+    {
+        return view(404);
+    }
+}
