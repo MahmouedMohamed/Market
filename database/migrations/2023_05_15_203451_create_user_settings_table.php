27cfba6877c81551223e90b1eb53ead8e914e06e
Mahmoued Mohamed <mahmouedmartin222@yahoo.com>
2024-08-22 04:08:09 +0300

    Create User Setting Table

---
+++ database/migrations/2023_05_15_203451_create_user_settings_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_settings', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->primary('id');
+            $table->string('user_id');
+            $table->string('language')->nullable();
+            $table->string('theme_id')->nullable();
+            $table->timestamps();
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->foreign('theme_id')
+                ->references('id')
+                ->on('themes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_settings');
+    }
+}
