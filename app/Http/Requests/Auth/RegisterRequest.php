c068513578874d57e4ee585d2145ecd8df951733
Mahmoued Mohamed <mahmouedmartin222@yahoo.com>
2024-08-22 05:27:14 +0300

    Add Register Request

---
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use App\ConverterModels\Gender;
+use App\Http\Requests\BaseRequest;
+use Illuminate\Validation\Rule;
+
+class RegisterRequest extends BaseRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+            'gender' => ['required', Rule::in(array_values(Gender::$text))],
+            'phone_number' => 'required',
+            'address' => 'string|max:1024',
+            'image' => 'image',
+            'nationality_id' => ['required', 'exists:nationalities,id'],
+            'type_id' => ['required', 'exists:user_types,id'],
+            'sub_type_id' => ['nullable', 'exists:user_sub_types,id'],
+            'theme_id' => ['nullable', 'exists:themes,id'],
+        ];
+    }
+}
