a874e63d1f808df10ab7660f10eecb70d9fd1e58
Mahmoued Mohamed <mahmouedmartin222@yahoo.com>
2024-08-22 04:08:20 +0300

    Create Access Token Table

---
+++ database/migrations/2024_08_22_124710_create_access_tokens_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAccessTokensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('oauth_access_tokens', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->primary('id');
+            $table->string('user_id');
+            $table->string('app_type');
+            $table->string('access_type');
+            $table->boolean('active')->default(1);
+            $table->date('expires_at')->nullable();
+            $table->timestamps();
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('oauth_access_tokens');
+    }
+}
