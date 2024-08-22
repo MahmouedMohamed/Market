ad66e193a33222c852376ce9ae401094bcf73474
Mahmoued Mohamed <mahmouedmartin222@yahoo.com>
2024-08-22 04:18:04 +0300

    Set Timestamps with false in translations

--- app/Models/UserSubTypeTranslation.php
+++ app/Models/UserSubTypeTranslation.php
@@ -6,6 +6,8 @@ class UserSubTypeTranslation extends BaseModel
 {
     public $table = 'user_sub_types_translations';
 
+    public $timestamps = false;
+
     public $fillable = [
         'name',
         'description',
--- app/Models/UserTypeTranslation.php
+++ app/Models/UserTypeTranslation.php
@@ -6,6 +6,8 @@ class UserTypeTranslation extends BaseModel
 {
     public $table = 'user_types_translations';
 
+    public $timestamps = false;
+
     public $fillable = [
         'name',
         'description',
